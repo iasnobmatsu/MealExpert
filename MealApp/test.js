49679d3a5a1849a00b2bcb1cbed40e4b5a017fa6
Clara Xu <xuclara98@gmail.com>
2019-10-13 18:37:10 -0400

    signup/login add files

---
+++ MealApp/test.js
@@ -0,0 +1,55 @@
+
+// import Axios from "axios";
+
+
+//testing
+// export async function fn1() {
+
+//     const result = await Axios({
+//         method: 'get',
+//         url: 'http://localhost:3000/public/authors/',
+//     })
+//     // result = JSON.stringify(result, null, 4);
+//     console.log(result);
+
+// };
+
+// export async function fn2() {
+//     try{
+//         const result = await Axios({
+//             method: 'post',
+//             url: 'http://localhost:3000/public/authors/',
+//             data: {
+//                 data: "Lord of the Rings"
+
+//             }
+//         })
+//         // result = JSON.stringify(result);
+//         console.log(result);
+//     }catch(error){
+//         console.log(error);
+//     }
+
+//     // axios.post('http://localhost:3000/public/', {
+//     //         username: 'api',
+//     //         password: 'MY_PASSWORD',
+//     //         grant_type: 'MY_GRANT_TYPE'
+//     //     }, {
+//     //         headers: {
+//     //             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+//     //         }
+//     //     })
+//     //     .then(response => {
+//     //         console.log(response)
+//     //     })
+//     //     .catch(error => {
+//     //         console.log(error.response)
+//     //     });
+
+
+
+
+
+// }
+// fn2()
+// fn1()
