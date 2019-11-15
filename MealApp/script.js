008eea6ef7573b05865a1437cda8a3ffc8b7ece1
Clara Xu <xuclara98@gmail.com>
2019-11-14 20:58:49 -0500

    add analysis

--- MealApp/script.js
+++ MealApp/script.js
@@ -3,8 +3,13 @@ let newjwt;
 let useDatabase;
 $(document).ready(() => {
     newjwt = '';
-    useDatabase=true;
+    useDatabase = true;
     renderlogin();
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    // google.charts.setOnLoadCallback(drawChart);
+
 
 });
 
@@ -226,21 +231,6 @@ export async function loginOnClick() {
     let password = $('#login-password').val();
     let loginreturn = await loginUser(username, password);
 
-    if (loginreturn != 'error') {
-        newjwt = loginreturn.data.jwt;
-        await renderRecord();
-
-    }
-
-}
-
-
-$('body').on('click', '#login', loginOnClick);
-
-
-//record================================================================================================\
-export async function renderRecord() {
-
     let today = new Date();
     let year = today.getFullYear();
     let month = today.getMonth() + 1;
@@ -254,32 +244,53 @@ export async function renderRecord() {
     today = '' + year + month + day;
     let defaultdate = "" + year + "-" + month + "-" + day;
 
+    if (loginreturn != 'error') {
+        newjwt = loginreturn.data.jwt;
+        await renderRecord(defaultdate, today);
+
+    }
+
+}
+
+
+$('body').on('click', '#login', loginOnClick);
+
+
+//record================================================================================================\
+export async function renderRecord(defaultdate, today) {
+
+
+
     $('#root').empty();
     $('#root').append($(`
     <div class="menu">
 
     <ul class="menu-list">
-        <li class='active has-text-weight-bold'><a>Record</a></li>
+        <li class='active has-text-weight-bold'><a id='record'>Record</a></li>
         <li><a>Planning</a></li>
-        <li><a>Analysis</a></li>
+        <li><a id='analysis'>Analysis</a></li>
     </ul>
 
 </div>
 
+
+
 <div class='container' id='appcont'>
-    <h3 class='title has-text-centered'>Meal Expert</h3>
 
-    <div id="#selectdate" class="forms-body has-text-centered">
-        <div class="field">
-            <label class="label"> Select A Date to View Meals</label>
-            <p class="control has-text-centered has-icons-left">
-                    <i class="fas fa-calendar-day"></i>
-                <input id='dateinput' type="date" value=${defaultdate}>
+<h3 class='title has-text-centered'>Meal Expert</h3>
 
-            </p>
+<div id="selectdate" class="forms-body has-text-centered">
+    <div class="field">
+        <label class="label"> Select A Date to View Meals</label>
+        <p class="control has-text-centered has-icons-left">
+                <i class="fas fa-calendar-day"></i>
+            <input id='dateinput' type="date" value=${defaultdate}>
 
-        </div>
+        </p>
+
     </div>
+</div>
+   
 
     <div class='forms-body editform'>
     <div class="tabs is-centered">
@@ -367,13 +378,13 @@ export async function rendermeals(jwt, date) {
             <td>${meals[mealkeys[i]].items[j].amount} grams</td>
             </tr>`))
         }
-        
-        let adddelete=$(`<div class='${mealkeys[i]}-cont meals-cont'><button data-date=${date} data-meal=${mealkeys[i]} class=" deletemeal is-outlined button is-small is-dark">Delete</button></div>`);
+
+        let adddelete = $(`<div class='${mealkeys[i]}-cont meals-cont'><button data-date=${date} data-meal=${mealkeys[i]} class=" deletemeal is-outlined button is-small is-dark">Delete</button></div>`);
         adddelete.append(onemeal);
         cont.append(adddelete);
     }
 
-    
+
 
     $('#rendercont').empty().append(cont)
 }
@@ -454,20 +465,54 @@ export async function createAddMealRecord(jwt, date, type, food, amount, calorie
 }
 
 
+//use this one, does not override all
+//add food object for a date type
+export async function createAddMealRecordExt(jwt, date, type, food, amount, calorie, nutrients, foodid) {
+    try {
+        const result = await axios({
+            method: 'post',
+            headers: {
+                "Authorization": "Bearer " + jwt
+            },
+            url: 'http://localhost:3000/user/record/' + date + '/' + type + '/items',
+            data: {
+                "type": "merge",
+                "data": {
+                    food,
+                    calorie,
+                    amount,
+                    foodid,
+                    nutrients
+                },
 
-$('body').on('click', '#addmeal', async () => {
-    let datearray = $('#dateinput').val().match(/[0-9]*/g);
-    let date = datearray.reduce(function reducer(acc, cur) {
-        return acc + cur;
-    }, "");
-    let food = $('#food').val();
-    let cal = $('#cal').val();
-    let am = $('#amount').val();
-    let type = $('#type option:selected').text();
-    createAddMealRecord(newjwt, date, type, food, am, cal);
-    // let meals = await getMeals(newjwt, 20191109);
-    // await rendermeals(newjwt, date);
-});
+            }
+
+        })
+        // console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+
+    rendermeals(jwt, date);
+}
+
+
+
+
+// $('body').on('click', '#addmeal', async () => {
+//     createAddMealRecordExt(jwt, date, type, food, amount, calorie, nutrients, foodid)
+//     let datearray = $('#dateinput').val().match(/[0-9]*/g);
+//     let date = datearray.reduce(function reducer(acc, cur) {
+//         return acc + cur;
+//     }, "");
+//     let food = $('#food').val();
+//     let cal = $('#cal').val();
+//     let am = $('#amount').val();
+//     let type = $('#type option:selected').text();
+//     createAddMealRecord(newjwt, date, type, food, am, cal);
+//     // let meals = await getMeals(newjwt, 20191109);
+//     // await rendermeals(newjwt, date);
+// });
 
 $('body').on('change', '#dateinput', async () => {
     let datearray = $('#dateinput').val().match(/[0-9]*/g);
@@ -490,7 +535,7 @@ export async function deleteWholeRecord(jwt) {
 }
 export async function deleteOneMeal(jwt, date, meal) {
     let result = await axios({
-        url: 'http://localhost:3000/user/record/'+date+"/"+meal,
+        url: 'http://localhost:3000/user/record/' + date + "/" + meal,
         headers: {
             "Authorization": "Bearer " + jwt
         },
@@ -500,11 +545,12 @@ export async function deleteOneMeal(jwt, date, meal) {
 }
 
 
-$('body').on('click','.deletemeal',async(e)=>{
-    let date=e.target.dataset.date;
-    let meal=e.target.dataset.meal;
+$('body').on('click', '.deletemeal', async (e) => {
+    let date = e.target.dataset.date;
+    let meal = e.target.dataset.meal;
+    // console.log(date.slice(0,4)+'-'+date.slice(4,6)+'-'+date.slice(6,8));
     await deleteOneMeal(newjwt, date, meal);
-    await renderRecord();
+    await rendermeals(newjwt, date);
 
 });
 
@@ -514,7 +560,7 @@ $('body').on('click','.deletemeal',async(e)=>{
 //edamam
 //it has custom auto-complete function
 
-export async function getAppid(){
+export async function getAppid() {
     let result = await axios({
         method: 'get',
         url: "http://localhost:3000/public/appid",
@@ -522,7 +568,7 @@ export async function getAppid(){
     return result.data.result;
 }
 
-export async function getKey(){
+export async function getKey() {
     let result = await axios({
         method: 'get',
         url: "http://localhost:3000/public/key",
@@ -532,9 +578,9 @@ export async function getKey(){
 
 //get food autocomplete
 export async function getFoodAuto(food_item) {
-    let key=await getKey();
-    let appid=await getAppid();
-    
+    let key = await getKey();
+    let appid = await getAppid();
+
     let result = await axios({
         method: 'get',
         url: "http://api.edamam.com/auto-complete",
@@ -551,9 +597,9 @@ export async function getFoodAuto(food_item) {
 
 //get food w/ nu
 export async function getFoodExternal(food_item) {
-    let key=await getKey();
-    let appid=await getAppid();
-    
+    let key = await getKey();
+    let appid = await getAppid();
+
     let result = await axios({
         method: 'get',
         url: "https://api.edamam.com/api/food-database/parser",
@@ -569,9 +615,9 @@ export async function getFoodExternal(food_item) {
 
 //use this method for get nutrients--not finished yet
 export async function getNutExternal(food_item) {
-    let key=await getKey();
-    let appid=await getAppid();
-    
+    let key = await getKey();
+    let appid = await getAppid();
+
     let result = await axios({
         method: 'get',
         url: "https://api.edamam.com/api/food-database/nutrients",
@@ -585,33 +631,292 @@ export async function getNutExternal(food_item) {
     return result;
 }
 
-
-$('body').on('keyup','#food',async()=>{
+//autocomplete
+$('body').on('keyup', '#food', async () => {
     // console.log($('#food').val());
-    let fillarray=await getFoodAuto($('#food').val());
-    let fill=$(`<div></div>`);
-    if (fillarray.length!=0){
-        for (let i=0;i<fillarray.length;i++){
+    let fillarray = await getFoodAuto($('#food').val());
+    let fill = $(`<div></div>`);
+    if (fillarray.length != 0) {
+        for (let i = 0; i < fillarray.length; i++) {
             fill.append($(`<p class='auto-item'> ${fillarray[i]}</p>`));
         }
     }
-   
+
     $('#food').parents('.field').find('.auto-cont').empty().append(fill);
 
 });
 
-//testing
 
-$('body').on('click','.auto-item',(e)=>{
+
+let nutr;
+let fid;
+let fname;
+$('body').on('click', '.auto-item', async (e) => {
+    fname = e.currentTarget.innerHTML;
     $('#food').val(e.currentTarget.innerHTML);
     $('.auto-cont').empty();
+    let result = await getFoodExternal(fname);
+    fid = result[0].food.foodId;
+    nutr = result[0].food.nutrients;
+
+    console.log(fid, nutr);
+
 });
 
+$('body').on('keyup', '#amount', async (e) => {
+    let am = parseInt($('#amount').val());
+    let cper100g = parseInt(nutr.ENERC_KCAL);
+    $('#cal').val(Math.floor(am * cper100g / 100));
+});
+
+$('body').on('click', '#addmeal', async () => {
+
+    let datearray = $('#dateinput').val().match(/[0-9]*/g);
+    let date = datearray.reduce(function reducer(acc, cur) {
+        return acc + cur;
+    }, "");
+    let food = $('#food').val();
+    let cal = $('#cal').val();
+    let am = $('#amount').val();
+    let type = $('#type option:selected').text();
+    // createAddMealRecord(newjwt, date, type, food, am, cal);
+    createAddMealRecordExt(newjwt, date, type, food, am, cal, nutr, fid)
+    // let meals = await getMeals(newjwt, 20191109);
+    // await rendermeals(newjwt, date);
+});
+
+
+//testing
+
 $('body').on('click', '#exte', async () => {
-    await getFoodExternal('california rolls');
+    // await getFoodExternal('california rolls');
     // await deleteWholeRecord(newjwt);
     // await getAppid();
     // await getKey();
 
     // await getFoodAuto('ap');
+});
+
+
+//chars-----------------------------------------------------------------------
+export function drawChart(dataArr) {
+
+    //   let data = google.visualization.arrayToDataTable([
+    //     ['Task', 'Hours per Day'],
+    //     ['Work',     11],
+    //     ['Eat',      2],
+    //     ['Commute',  2],
+    //     ['Watch TV', 2],
+    //     ['Sleep',    7]
+    //   ]);
+
+    let data = google.visualization.arrayToDataTable(dataArr);
+
+
+    let options = {
+        title: 'Your Daily Nutritions/grams'
+    };
+
+    let chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+
+
+}
+
+
+//switch tabs--------------
+$('body').on('click', '#analysis', () => {
+
+    $('#analysis').parents('li').addClass('active');
+    $('#analysis').parents('li').addClass('has-text-weight-bold');
+    $('#record').parents('li').removeClass('active');
+    $('#record').parents('li').removeClass('has-text-weight-bold');
+    let defaultdate = $('#dateinput').val();
+    $('#appcont').empty();
+
+    // let today = new Date();
+    // let year = today.getFullYear();
+    // let month = today.getMonth() + 1;
+    // if (month < 10) {
+    //     month = '0' + month;
+    // }
+    // let day = today.getDate();
+    // if (day < 10) {
+    //     day = '0' + day;
+    // }
+    // today = '' + year + month + day;
+    // ldefaultdate = "" + year + "-" + month + "-" + day;
+
+    $('#appcont').append($(`
+    <h3 class='title has-text-centered'>Meal Expert</h3>
+
+    <div id="selectdate" class="forms-body has-text-centered">
+        <div class="field">
+            <label class="label"> Select A Date to View Meals</label>
+            <p class="control has-text-centered has-icons-left">
+                    <i class="fas fa-calendar-day"></i>
+                <input id='dateinputAnal' type="date" value=${defaultdate}>
+
+            </p>
+
+        </div>
+    </div>
+    
+    <div id='nutrtablecont'></div>
+    `));
+    let datearray = defaultdate.match(/[0-9]*/g);
+    let date = datearray.reduce(function reducer(acc, cur) {
+        return acc + cur;
+    }, "");
+
+
+    renderOneDayAnalysis(date);
+
+
+});
+
+$('body').on('click', '#record', () => {
+    $('#analysis').parents('li').removeClass('active');
+    $('#analysis').parents('li').removeClass('has-text-weight-bold');
+    $('#record').parents('li').addClass('active');
+    $('#record').parents('li').addClass('has-text-weight-bold');
+    let defaultdate = $('#dateinputAnal').val();
+    let datearray = defaultdate.match(/[0-9]*/g);
+    let date = datearray.reduce(function reducer(acc, cur) {
+        return acc + cur;
+    }, "");
+    $('#appcont').empty();
+    $('#piechart').html("");
+    renderRecord(defaultdate, date);
+
+
+});
+
+let conv = {
+    "CA": 1000,
+    "CHOCDF": 1000000,
+    "CHOLE": 1000,
+    "FAMS": 1000000,
+    "FAPU": 1000000,
+    "FASAT": 1000000,
+    "FAT": 1000000,
+    "FATRN": 1000000,
+    "FE": 1000,
+    "FIBTG": 1000000,
+    "FOLDFE": 1,
+    "K": 1000,
+    "MG": 1000,
+    "NA": 1000,
+    "NIA": 1000,
+    "P": 1000,
+    "PROCNT": 1000000,
+    "RIBF": 1000,
+    "SUGAR": 1000000,
+    "THIA": 1000,
+    "TOCPHA": 1000,
+    "VITA_RAE": 1,
+    "VITB12": 1,
+    "VITB6A": 1000,
+    "VITC": 1000,
+    "VITD": 1,
+    "VITK1": 1,
+    "ENERC_KCAL": 1
+}
+
+let convN = {
+    "CA": "Calcium",
+    "CHOCDF": "Carbs",
+    "CHOLE": "Cholesterol",
+    "FAMS": "Monounsaturated",
+    "FAPU": "Polyunsaturated",
+    "FASAT": "Saturated",
+    "FAT": "Fat",
+    "FATRN": "Trans",
+    "FE": "Iron",
+    "FIBTG": "Fiber",
+    "FOLDFE": "Folate ",
+    "K": "Potassium	",
+    "MG": "Magnesium",
+    "NA": "Sodium",
+    "NIA": "Niacin/B3",
+    "P": "Phosphorus",
+    "PROCNT": "Protein",
+    "RIBF": "Riboflavin/B2",
+    "SUGAR": "Sugars",
+    "THIA": "Thiamin/B1",
+    "TOCPHA": "Vitamin E",
+    "VITA_RAE": "Vitamin A",
+    "VITB12": "Vitamin B12",
+    "VITB6A": "Vitamin B6",
+    "VITC": "Vitamin C",
+    "VITD": "Vitamin D",
+    "VITK1": "Vitamin K",
+}
+
+export async function renderOneDayAnalysis(date) {
+    let meal = await getMeals(newjwt, date);
+    console.log(meal);
+
+    let nudic = {};
+    //   let nkarr=;
+    let mealkeys = Object.keys(meal);
+    for (let i = 0; i < mealkeys.length; i++) {
+        for (let j = 0; j < meal[mealkeys[i]].items.length; j++) {
+            let item = meal[mealkeys[i]].items[j];
+            let amt = item.amount;
+            let nutrs = item.nutrients;
+            // console.log(nutrs);
+            let nutkeys = Object.keys(nutrs);
+
+            for (let a = 0; a < nutkeys.length; a++) {
+                if (Object.keys(nudic).includes(nutkeys[a])) {
+                    nudic[nutkeys[a]] = nudic[nutkeys[a]] + parseInt(nutrs[nutkeys[a]]) * amt * conv[nutkeys[a]] / 100;
+                } else {
+                    nudic[nutkeys[a]] = parseInt(nutrs[nutkeys[a]]) * amt * conv[nutkeys[a]] / 100;
+                }
+            }
+
+
+        }
+    }
+
+    let dataArr = [];
+    dataArr.push(['nutrient', 'cnt']);
+    for (let i = 0; i < Object.keys(nudic).length; i++) {
+        if (Object.keys(nudic)[i] != "ENERC_KCAL") {
+            dataArr.push([convN[Object.keys(nudic)[i]], nudic[Object.keys(nudic)[i]] / 1000000]);
+        }
+    }
+
+    //  console.log(dataArr);
+
+    drawChart(dataArr);
+
+    dataArr.push(['Calories', nudic['ENERC_KCAL']]);
+
+    drawtable(dataArr);
+
+}
+
+export function drawtable(dataArr) {
+
+    let table = $(`<table class='nutrtable'>
+    <tr class="heading-table"><th>Nutrients</th><td>Total Amount</td></tr></table>`);
+    for (let j = 1; j < dataArr.length; j++) {
+        table.append($(`<tr>
+    <th>${dataArr[j][0]}</th>
+    <td>${dataArr[j][1]}</td>
+    </tr>`))
+    }
+    $('#nutrtablecont').empty().append(table);
+}
+
+
+$('body').on('change', '#dateinputAnal', async () => {
+    let datearray = $('#dateinputAnal').val().match(/[0-9]*/g);
+    let date = datearray.reduce(function reducer(acc, cur) {
+        return acc + cur;
+    }, "");
+    await renderOneDayAnalysis(date);
 });
